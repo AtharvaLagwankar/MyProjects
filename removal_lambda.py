477ed9d276238c430d8605ca74f163fa22227b0f
AtharvaLagwankar <63859488+AtharvaLagwankar@users.noreply.github.com>
2020-04-17 22:19:10 +0530

    Add files via upload

---
+++ removal_lambda.py
@@ -0,0 +1,10 @@
+my_list = []
+print("ENTER NUMBERS FOR THE LIST (TYPE 'q' TO STOP ENTRY)")
+w = input()
+while w != "q":
+    my_list.append(float(w))
+    w = input()
+print(my_list)
+num = input("ENTER THE VALUE TO BE REMOVED FROM THE DISPLAYED LIST :")
+    my_list = list(filter(lambda x: x != float(num), my_list))
+print("THE FINAL LIST IS : ", my_list)
