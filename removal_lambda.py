c72d033f16293e2328be1fd3fe02b44e375bdf01
AtharvaLagwankar <63859488+AtharvaLagwankar@users.noreply.github.com>
2020-04-17 22:43:18 +0530

    Update removal_lambda.py

--- removal_lambda.py
+++ removal_lambda.py
@@ -1,10 +1,5 @@
-my_list = []
-print("ENTER NUMBERS FOR THE LIST (TYPE 'q' TO STOP ENTRY)")
-w = input()
-while w != "q":
-    my_list.append(float(w))
-    w = input()
-print(my_list)
+print("ENTER NUMBERS FOR THE LIST (SEPARATED BY SPACE) : ")
+m = input().split()
 num = input("ENTER THE VALUE TO BE REMOVED FROM THE DISPLAYED LIST :")
-my_list = list(filter(lambda x: x != float(num), my_list))
+my_list = list(filter(lambda x: x != num, m))
 print("THE FINAL LIST IS : ", my_list)
