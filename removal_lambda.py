3d418b6df5f529644ba5b7453b5de84e0f77f471
AtharvaLagwankar <63859488+AtharvaLagwankar@users.noreply.github.com>
2020-04-17 22:22:07 +0530

    Update removal_lambda.py

--- removal_lambda.py
+++ removal_lambda.py
@@ -6,5 +6,5 @@ while w != "q":
     w = input()
 print(my_list)
 num = input("ENTER THE VALUE TO BE REMOVED FROM THE DISPLAYED LIST :")
-    my_list = list(filter(lambda x: x != float(num), my_list))
+my_list = list(filter(lambda x: x != float(num), my_list))
 print("THE FINAL LIST IS : ", my_list)
